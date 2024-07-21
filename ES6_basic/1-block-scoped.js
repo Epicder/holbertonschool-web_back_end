061daf1a93f3d42c2961fdaf71d259b5ffdf6445
Epicder <epicder11@gmail.com>
2024-07-21 03:06:48 -0300

    task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -5,7 +5,8 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
-    }
+    return [task, task2];
+  }
 
   return [task, task2];
 }
