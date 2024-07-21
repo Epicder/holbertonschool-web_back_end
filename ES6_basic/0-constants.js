97a48e2287ea51be201f993b0ed1b035670b580f
Epicder <epicder11@gmail.com>
2024-07-21 02:52:00 -0300

    lint fix 2

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -2,14 +2,14 @@ export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
-  
+
 export function getLast() {
   return ' is okay';
 }
-  
+
 export function taskNext() {
   let combination = 'But sometimes let';
-  combination += getLast();
-  
+  combination += getLast(); 
+
   return combination;
 }
