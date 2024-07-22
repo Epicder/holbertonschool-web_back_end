583c1fd876f91d81fbbfe0923b3918ae772904a0
Epicder <epicder11@gmail.com>
2024-07-21 22:43:31 -0300

    linter fix

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,8 +1,8 @@
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
-  }
-  
+}
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const currentYear = getCurrentYear();
   const budget = {
