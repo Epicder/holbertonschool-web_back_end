fdadac63292dc6481d9263362d0e8c1b35f50d42
Epicder <epicder11@gmail.com>
2024-07-25 13:39:29 -0300

    fix

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
     constructor(code, name) {
-        this._code = code;
-        this._name = name;
+        this.code = code;
+        this.name = name;
     } 
   get code() {
     return this._code;
