d72e976e7f107ab59fc9b13e926aa542715634e4
Epicder <epicder11@gmail.com>
2024-07-25 13:26:39 -0300

    task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -32,9 +32,9 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = students;
-    }
+  }
 }
