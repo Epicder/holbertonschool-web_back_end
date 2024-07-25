8d742d4d4a3d8ad90ceced7dbddecdab5cb325ec
Epicder <epicder11@gmail.com>
2024-07-25 13:45:14 -0300

    task 3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -6,8 +6,21 @@ export default class Currency {
   get code() {
     return this._code;
   }
+
   get name() {
     return this._name;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    }
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
   }
 
   displayFullCurrency(name, code) {
