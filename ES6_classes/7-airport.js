9c91945f3a238cb80b0cee6ecaf38908fe375323
Epicder <epicder11@gmail.com>
2024-07-26 00:45:56 -0300

    task 7

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    }
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
