12de09b789b23d0797a119c6207969a200987cf6
Epicder <epicder11@gmail.com>
2024-07-26 01:50:55 -0300

    try 2

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -35,10 +35,12 @@ export class StudentHolberton {
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
