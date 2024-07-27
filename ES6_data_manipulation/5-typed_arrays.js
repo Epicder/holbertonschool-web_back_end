30929af72a783773618fe359ba64cdd54ce41cfe
Epicder <epicder11@gmail.com>
2024-07-27 15:01:22 -0300

    task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+  
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  int8Array[position] = value;
+  return buffer;
+}
