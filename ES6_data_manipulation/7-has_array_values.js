8d2c99a01c37c4ac840b2d40bff4236258ea8cbc
Epicder <epicder11@gmail.com>
2024-07-27 15:14:43 -0300

    sets doesnt have lenght property lmao

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,8 @@
 export default function hasValuesFromArray(set, array) {
-  const i = 0
- for (i = 0; i <= set.length; i++) {
-   if (set[i] === array[i]) {
-     return true;
-   }
- }
- return false;
+  for (const i of array) {
+    if (!set.has(i)) {
+        return false;
+    }
+  }
+  return true;
 }
