345dfe2ac67e7b5eb366ce95dac2b8cb3a4ea1dd
Epicder <epicder11@gmail.com>
2024-07-27 15:31:41 -0300

    task 8 fix?

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startSting) {
   const result = [];
-
+  if (startSting.length === 0 || startSting !== (typeof 'string')) {
+    return '';
+  }
+  
   for (const i of set) {
     if (i.startsWith(startSting)) {
       result.push(i.slice(startSting.length));
