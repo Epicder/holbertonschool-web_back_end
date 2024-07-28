d3e097b102154f890a4063457e0e59f8f4c01a64
Epicder <epicder11@gmail.com>
2024-07-28 19:13:33 -0300

    TASK 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve('Ok');
     }, 2000);
