7ab58ef63d5fdf491a93040b5875e0a61eee7856
Epicder <epicder11@gmail.com>
2024-07-28 18:15:59 -0300

    fix lint 2

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 export default function updateUniqueItems(groceries) {
   if (!(groceries instanceof Map)) {
     throw new Error('Cannot process');
-}
-
-for (const [key, value] of groceries.entries()) {
-  if (value === 1) {
-    groceries.set(key, 100);
   }
-}
-return groceries;
+
+  for (const [key, value] of groceries.entries()) {
+    if (value === 1) {
+      groceries.set(key, 100);
+    }
+  } 
+  return groceries;
 }
