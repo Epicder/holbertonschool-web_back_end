d51aad61ded955436fd7c48d27f05ebdf03ce409
Epicder <epicder11@gmail.com>
2024-07-28 17:41:30 -0300

    task 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const output = new Map();
+  output.set('Apples', 10);
+  output.set('Tomatoes', 10);
+  output.set('Pasta', 1);
+  output.set('Rice', 1);
+  output.set('Banana', 5);
+  return output;
+}
