8a7aae45ab56d3e5f0fd3edc6e1d3afbdfa93404
Epicder <epicder11@gmail.com>
2024-07-28 18:14:14 -0300

    fix lint

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -4,9 +4,9 @@ export default function updateUniqueItems(groceries) {
 }
 
 for (const [key, value] of groceries.entries()) {
-    if (value === 1) {
-        groceries.set(key, 100);
-    }
+  if (value === 1) {
+    groceries.set(key, 100);
+  }
 }
 return groceries;
 }
