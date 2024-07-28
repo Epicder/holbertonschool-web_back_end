89ff16b4e45fbc0b9088d623fadf7d9aac498427
Epicder <epicder11@gmail.com>
2024-07-28 19:42:55 -0300

    tas 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({
         status: 200,
-        body: 'Success'
+        body: 'Success',
       });
     } else {
       reject(new Error('The fake API is not working currently'));
