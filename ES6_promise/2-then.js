75da068e6e44e16cb1cfd7b63f3af84d6a650289
Epicder <epicder11@gmail.com>
2024-07-28 20:47:35 -0300

    lint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'success',}))
+    .then(() => ({
+       status: 200,
+       body: 'success',
+    }))
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
