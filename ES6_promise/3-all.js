67183995f9021e8e44f554aba4e5bf0d67ddc108
Epicder <epicder11@gmail.com>
2024-07-28 23:02:50 -0300

    "Updated import statement in 3-all.js to remove '.js' extension from './utils' module"

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
