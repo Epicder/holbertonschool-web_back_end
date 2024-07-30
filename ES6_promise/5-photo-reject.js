e9b09924a4ef0541b09c6f3e3a54efc2d08efda7
Epicder <epicder11@gmail.com>
2024-07-30 10:05:04 -0300

    param

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
-    return new Promise((reject) => {
+    return new Promise((_, reject) => {
       reject(new Error(`${fileName} cannot be processed`));
       });
 }
