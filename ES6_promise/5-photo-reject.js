3ac6f842d914f0fa5408a4f0907c219afae9ed03
Epicder <epicder11@gmail.com>
2024-07-30 10:06:56 -0300

    fix lint

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
-    return new Promise((_, reject) => {
-      reject(new Error(`${fileName} cannot be processed`));
-      });
+  return new Promise((_, reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
+  });
 }
