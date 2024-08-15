eabbcf939710d30cfe470d2d6cbbe53e58ff59bd
Epicder <epicder11@gmail.com>
2024-08-15 20:34:32 -0300

    "Formatted docstrings in make_multiplier.py"

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-"""typed function that takes a float multiplier as argument and returns a function that multiplies a float by multiplier."""
+"""typed function that takes a float multiplier as
+argument and returns a function that multiplies a float by multiplier."""
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """typed function that takes a float number as argument and returns the product of number and multiplier."""
+    """typed function that takes a float number as argument and
+      returns the product of number and multiplier."""
     def multiply(x: float) -> float:
         """the function that will be returned"""
         return x * multiplier
