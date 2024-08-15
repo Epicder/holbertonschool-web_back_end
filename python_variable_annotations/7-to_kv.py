7fdab3d2bf7310fbefb68e3e611ea58cd26957ad
Epicder <epicder11@gmail.com>
2024-08-15 19:40:12 -0300

    task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""takes a string k and an int OR float v as arguments and returns a tuple"""
+
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple [str, float]:
+    """Return a tuple of key-value pair."""
+    return (k, float(v**2))
