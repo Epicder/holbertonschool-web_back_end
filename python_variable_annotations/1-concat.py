e9abc038614c5ef2b3fa16cadc66e65d955592f9
Epicder <epicder11@gmail.com>
2024-08-15 09:57:43 -0300

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns a concated str
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns a concated str"""
+    return str1 + str2
