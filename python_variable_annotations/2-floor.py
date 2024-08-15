c933ab5412f500a222a7becddaf85da0f39f92f1
Epicder <epicder11@gmail.com>
2024-08-15 10:00:59 -0300

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module returns the floor of the float n."""
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """returns the floor of the float n."""
+    return math.floor(n)
