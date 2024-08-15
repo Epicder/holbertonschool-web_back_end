b85325f420222b1d17f0cabfc59e164f5f549869
Epicder <epicder11@gmail.com>
2024-08-15 14:32:53 -0300

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+takes a list input_list of floats as argument and returns their sum
+"""
+
+
+def sum_list(input_list: float) -> float:
+    """sum a passed list"""
+    return sum(input_list)
