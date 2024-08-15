0e0b56a4d3b03eb1a167ba0206cce7333470fff6
Epicder <epicder11@gmail.com>
2024-08-15 19:22:18 -0300

    documentation

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import typing
 """
 takes a list mxd_lst of integers and floats and returns their sum
 """
+
+import typing
 
 
 def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
