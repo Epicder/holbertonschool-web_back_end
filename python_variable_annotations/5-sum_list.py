da33aa517b4c9d2fe5569b76f9761ffae56a5b04
Epicder <epicder11@gmail.com>
2024-08-15 14:36:30 -0300

    fix

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -6,6 +6,6 @@ takes a list input_list of floats as argument and returns their sum
 import typing
 
 
-def sum_list(input_list: typing.List[float]) -> typing.List[float]:
+def sum_list(input_list: typing.List[float]) -> float:
     """sum a passed list"""
     return sum(input_list)
