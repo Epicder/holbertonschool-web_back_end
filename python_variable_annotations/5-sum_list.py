ff3f696c59c6d79dcdb9807278e0152ff636673a
Epicder <epicder11@gmail.com>
2024-08-15 14:34:08 -0300

    fix

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,6 +5,6 @@ takes a list input_list of floats as argument and returns their sum
 """
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: float) -> list:
     """sum a passed list"""
     return sum(input_list)
