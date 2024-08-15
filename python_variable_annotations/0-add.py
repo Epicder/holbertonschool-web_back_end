23ee530bebc8f05e8c98d9364140e28316794dc5
Epicder <epicder11@gmail.com>
2024-08-15 09:49:46 -0300

    fix documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+This module contains the function to add two floats.
+"""
 def add(a: float, b: float) -> float:
     """Return the sum of a and b in float."""
     return a + b
