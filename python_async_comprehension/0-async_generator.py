805fbfa313519bf0c1b18cffcd285c7f264987c0
Epicder <epicder11@gmail.com>
2024-08-20 11:54:45 -0300

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""this module give 10 random numbers from 0 to 10"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """loop 10 times and yields a random number every time"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randrange(0, 10)
+        
