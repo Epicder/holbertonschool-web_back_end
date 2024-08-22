847bfb8fba0fb1a8fc6856d75343e8fc328d8396
Epicder <epicder11@gmail.com>
2024-08-22 13:23:10 -0300

    fix?

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -9,6 +9,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> typing.List[float]:
     """Uses async for to iterate"""
-    async for _ in range(10):
-        await asyncio.sleep(1)
-        return random.randrange(0, 10)
+    return [number async for number in async_generator()]
