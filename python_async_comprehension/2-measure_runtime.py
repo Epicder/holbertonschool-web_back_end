47bd6028adef28a6ea21e88b67aeaaae098c9cc6
Epicder <epicder11@gmail.com>
2024-08-22 15:12:14 -0300

    "Updated import statement to access async_comprehension module"

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,7 +3,7 @@
 import asyncio
 import time
 import typing
-async_comprehension = __import__('1-async_comprehension')
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
