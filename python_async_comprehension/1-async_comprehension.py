8d0bb5f5f3ab2ae91de6845d2fcd3a180ed5364f
Epicder <epicder11@gmail.com>
2024-08-22 13:24:39 -0300

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -9,4 +9,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> typing.List[float]:
     """Uses async for to iterate"""
-    return [number async for number in async_generator()]
+    return [item async for item in async_generator()]
