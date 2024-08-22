b0a3e2de907878bff0fd2c124ad76d7cefef0c02
Epicder <epicder11@gmail.com>
2024-08-22 13:20:17 -0300

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+"""this module uses async comprehension to iterate"""
 import typing
 import asyncio
 import random
@@ -7,5 +8,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> typing.List[float]:
+    """Uses async for to iterate"""
     async for _ in range(10):
-     return random.randrange(0, 10)
+        await asyncio.sleep(1)
+        return random.randrange(0, 10)
