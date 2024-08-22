5d44b4357d3b6021e9f4f13eb0aa95be3547c142
Epicder <epicder11@gmail.com>
2024-08-22 12:50:26 -0300

    task 0 fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,11 +2,10 @@
 """this module give 10 random numbers from 0 to 10"""
 import asyncio
 import random
+import typing
 
-
-async def async_generator():
+async def async_generator() -> typing.AsyncIterator[float]:
     """loop 10 times and yields a random number every time"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.randrange(0, 10)
-        
