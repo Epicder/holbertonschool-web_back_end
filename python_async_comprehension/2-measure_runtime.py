25cd7e8f350bf38b282a6edc83de4a1d951728d7
Epicder <epicder11@gmail.com>
2024-08-22 15:08:34 -0300

    fix

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -10,8 +10,8 @@ async def measure_runtime() -> float:
     """measure the runtime of executing async_comprehension
     four times in parallel"""
     start_time = time.perf_counter()
-    await asyncio.gather(async_comprehension(), async_comprehension(),
-                         async_comprehension(), async_comprehension())
+    await asyncio.gather(async_comprehension, async_comprehension,
+                         async_comprehension, async_comprehension)
 
     total_time = time.perf_counter() - start_time
     return total_time
