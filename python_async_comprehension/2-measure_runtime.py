8c2cf74b0e14f35ef1a9ff317a3da0a26fab06d0
Epicder <epicder11@gmail.com>
2024-08-22 15:10:51 -0300

    "Refactor measure_runtime function to use a generator expression with asyncio.gather"

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -10,8 +10,6 @@ async def measure_runtime() -> float:
     """measure the runtime of executing async_comprehension
     four times in parallel"""
     start_time = time.perf_counter()
-    await asyncio.gather(async_comprehension, async_comprehension,
-                         async_comprehension, async_comprehension)
-
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     total_time = time.perf_counter() - start_time
     return total_time
