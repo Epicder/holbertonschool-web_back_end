2f90bbfe8527e7f6a01171cbc3cd52b4991c321e
Epicder <epicder11@gmail.com>
2024-09-20 03:21:45 -0300

    lint fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-  process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 process.stdin.on('end', () => {
