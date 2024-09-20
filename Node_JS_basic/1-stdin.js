a52a61a25fdc6dca22629c254baa67d39b3e310c
Epicder <epicder11@gmail.com>
2024-09-20 02:56:47 -0300

    added line breaks now on stdout manually

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 
 process.stdin.on('data', (data) => {
@@ -9,5 +9,5 @@ process.stdin.on('data', (data) => {
 });
 
 process.on('exit', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
