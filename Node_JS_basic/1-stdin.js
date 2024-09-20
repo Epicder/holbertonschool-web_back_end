8cbb5fc3c2e24ec0f5d929b75acb6c1cfe813b58
Epicder <epicder11@gmail.com>
2024-09-20 03:17:59 -0300

    \r deleted

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write(`Your name is: ${name}`);
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
