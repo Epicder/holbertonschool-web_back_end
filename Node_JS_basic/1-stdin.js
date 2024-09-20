a24b39f5c33e17c96b1473ade9bba28df1f6f379
Epicder <epicder11@gmail.com>
2024-09-20 03:14:25 -0300

    \r

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\r`);
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
