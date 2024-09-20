0e1b7576749ce927dd449aa6954e668f6de845f1
Epicder <epicder11@gmail.com>
2024-09-20 03:12:11 -0300

    fix 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,6 +3,6 @@ process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}`);
 });
-process.on('end', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
