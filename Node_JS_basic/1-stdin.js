e6890914891d6ec1160ea66f840b0fce01a3dec7
Epicder <epicder11@gmail.com>
2024-09-20 02:51:42 -0300

    changed con.log for stdout

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?');
 
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   if (name !== null) {
-  console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing');
 });
