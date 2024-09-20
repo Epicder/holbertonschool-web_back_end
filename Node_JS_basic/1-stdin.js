bcfeb74f7b9a4152315d709c3a004e6f4de7e862
Epicder <epicder11@gmail.com>
2024-09-20 03:16:19 -0300

    Fix EOF 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
   process.stdout.write(`Your name is: ${name}\r`);
 });
 process.stdin.on('end', () => {
