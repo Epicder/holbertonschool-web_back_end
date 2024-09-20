62925711d8e32ca7213e28ff5d272e6e81093cca
Epicder <epicder11@gmail.com>
2024-09-20 02:25:06 -0300

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim(); 
+  console.log(`Your name is: ${name}`);
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
