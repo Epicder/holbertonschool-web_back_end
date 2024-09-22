52add3960bdd965f894c31a28064744d8a6be080
Epicder <epicder11@gmail.com>
2024-09-22 20:23:21 -0300

    task 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+function countStudents(path) {
+  const fs = require('fs');
+  try {
+
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\n').filter(line => line.trim().length > 0);
+    const students = lines.slice(1);
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+
+    students.forEach((line) => {
+      const [firstName, , , field] = line.split(',');
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    });
+
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
