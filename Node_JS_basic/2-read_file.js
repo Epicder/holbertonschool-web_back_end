bb502fd47e516ae8cac2fbf37375ad199b9edd09
Epicder <epicder11@gmail.com>
2024-09-22 20:35:52 -0300

    lint fix

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
-function countStudents(path) {
 
+function countStudents(path) {
   try {
+
     const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.split('\n').filter(line => line.trim().length > 0);
+    const lines = data.split('\n').filter(line => (line.trim()).length > 0);
     const students = lines.slice(1);
     console.log(`Number of students: ${students.length}`);
     const fields = {};
