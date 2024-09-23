24044a421410d3fa82e78b33f94c41df3c2e3efe
Epicder <epicder11@gmail.com>
2024-09-22 21:43:16 -0300

    task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const app = http.createServer((_, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
