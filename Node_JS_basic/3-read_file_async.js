d3502333c5a024ba9b8c554f4c3e93d906c42aec
Epicder <epicder11@gmail.com>
2024-09-29 20:44:37 -0300

    visual studio identation in js files fix

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,36 +1,36 @@
 const fs = require('fs').promises;
 
 async function countStudents(path) {
-    try {
-        const data = await fs.readFile(path, 'utf8');
-        
-        const lines = data.split('\n').filter(line => line.trim() !== '');
-        if (lines.length === 0) {
-            throw new Error('Cannot load the database');
-        }
+  try {
+      const data = await fs.readFile(path, 'utf8');
 
-        const studentsByField = {};
-        let totalStudents = 0;
+      const lines = data.split('\n').filter(line => line.trim() !== '');
+      if (lines.length === 0) {
+          throw new Error('Cannot load the database');
+      }
 
-        lines.slice(1).forEach((line) => {
-            const [firstname, lastname, age, field] = line.split(',');
-            if (!firstname || !lastname || !age || !field) {
-                return;
-            }
-            
-            if (!studentsByField[field]) {
+      const studentsByField = {};
+      let totalStudents = 0;
+
+      lines.slice(1).forEach((line) => {
+          const [firstname, lastname, age, field] = line.split(',');
+          if (!firstname || !lastname || !age || !field) {
+              return;
+          }
+
+          if (!studentsByField[field]) {
                 studentsByField[field] = [];
-            }
-            
-            studentsByField[field].push(firstname);
-            totalStudents += 1;
-        });
+          }
+ 
+          studentsByField[field].push(firstname);
+          totalStudents += 1;
+      });
 
-        console.log(`Number of students: ${totalStudents}`);
+      console.log(`Number of students: ${totalStudents}`);
 
-        for (const [field, students] of Object.entries(studentsByField)) {
-            console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-        }
+      for (const [field, students] of Object.entries(studentsByField)) {
+          console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      }
 
     } catch (error) {
         throw new Error('Cannot load the database');
